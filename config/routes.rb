1b088dade571987d8f7559d89cee61c22d16cd3d
Crunchyto <crunchyto@gmail.com>
2012-12-08 21:09:35 +0100

    Finalizado  layout y rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  
+  get "users/new"
+
+  root to: 'static_pages#home' 
+
+  match '/signup',  to: 'users#new'     # signup_path               
+
+  match '/help',    to: 'static_pages#help'     # help_path
+  match '/about',   to: 'static_pages#about'    # about_path
+  match '/contact', to: 'static_pages#contact'  # contact_path
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
